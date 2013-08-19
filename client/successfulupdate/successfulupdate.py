c012cef0e29b9b5624e3173d799077820bff7bac
Joshua Deare <josh.deare@coreos.com>
2013-08-19 15:18:24 -0700

    refactor project

---
+++ client/successfulupdate/successfulupdate.py
@@ -0,0 +1,16 @@
+from autotest.client import test, utils
+import dbus
+import time
+import os
+
+success = False
+
+class successfulupdate(test.test):
+    version = 1
+
+    def run_once(self):
+    	utils.system("cp " + self.srcdir + "/../../site_tests/successfulupdate/reboottesting.service /media/state/units/")
+	utils.system("sudo cgpt add -i 3 -P 5 /dev/sda")
+
+	#TODO change to prod update service
+	utils.system("update_engine_client -update -omaha_url http://192.168.122.1:8080/update")
